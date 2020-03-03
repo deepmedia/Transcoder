d541c4ca88e2375879635c93b306bd37de9df138
Mudar Noufal <mn@mudar.ca>
2020-03-03 09:18:36 -0500

    Updated muted audio mediaFormat values
    applied PR suggestions

--- lib/src/main/java/com/otaliastudios/transcoder/source/MutedAudioDataSource.java
+++ lib/src/main/java/com/otaliastudios/transcoder/source/MutedAudioDataSource.java
@@ -21,24 +21,28 @@ public class MutedAudioDataSource implements DataSource {
     private final static String TAG = MutedAudioDataSource.class.getSimpleName();
 
     private static final int CHANNEL_COUNT = 2;
-    private static final int BIT_RATE = 64000;
-    private final static int SAMPLE_RATE = 48000;
-    private final static int BUFFER_SIZE = 65536; // Lower values are ignored
-    private final static int PERIOD_SIZE = BUFFER_SIZE * CHANNEL_COUNT / 32;
-    private final static long PERIOD_TIME = 21333;
+    private static final int SAMPLE_RATE = 44100;
+    private static final int BITS_PER_SAMPLE = 16;
+    private static final int BIT_RATE = CHANNEL_COUNT * SAMPLE_RATE * BITS_PER_SAMPLE;
+    private static final int SAMPLES_PER_PERIOD = 2048;
+    private static final double PERIOD_TIME_SECONDS = (double) SAMPLES_PER_PERIOD / SAMPLE_RATE;
+    private static final long PERIOD_TIME_US = (long) (1000000 * PERIOD_TIME_SECONDS);
+    private static final int PERIOD_SIZE = (int) (PERIOD_TIME_SECONDS * BIT_RATE / 8);
 
-    private final MediaFormat audioFormat;
     private final long durationUs;
+    private final ByteBuffer byteBuffer;
+    private final MediaFormat audioFormat;
 
-    private long mCurrentTimestampUs = 0L;
+    private long currentTimestampUs = 0L;
 
     public MutedAudioDataSource(Long durationUs) {
         this.durationUs = durationUs;
+        this.byteBuffer = ByteBuffer.allocateDirect(PERIOD_SIZE).order(ByteOrder.nativeOrder());
         this.audioFormat = new MediaFormat();
         audioFormat.setString(MediaFormat.KEY_MIME, MIMETYPE_AUDIO_RAW);
         audioFormat.setInteger(MediaFormat.KEY_BIT_RATE, BIT_RATE);
         audioFormat.setInteger(MediaFormat.KEY_CHANNEL_COUNT, CHANNEL_COUNT);
-        audioFormat.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, BUFFER_SIZE);
+        audioFormat.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, PERIOD_SIZE);
         audioFormat.setInteger(MediaFormat.KEY_SAMPLE_RATE, SAMPLE_RATE);
     }
 
@@ -81,27 +85,28 @@ public class MutedAudioDataSource implements DataSource {
 
     @Override
     public void readTrack(@NonNull Chunk chunk) {
-        chunk.buffer = ByteBuffer.allocateDirect(BUFFER_SIZE).order(ByteOrder.nativeOrder());
+        byteBuffer.clear();
+        chunk.buffer = byteBuffer;
         chunk.isKeyFrame = true;
         if (isDrained()) {
             chunk.timestampUs = 0;
             chunk.bytes = -1;
         } else {
-            chunk.timestampUs = mCurrentTimestampUs;
+            chunk.timestampUs = currentTimestampUs;
             chunk.bytes = PERIOD_SIZE;
         }
 
-        mCurrentTimestampUs += PERIOD_TIME;
+        currentTimestampUs += PERIOD_TIME_US;
     }
 
     @Override
     public long getReadUs() {
-        return mCurrentTimestampUs;
+        return currentTimestampUs;
     }
 
     @Override
     public boolean isDrained() {
-        return mCurrentTimestampUs >= getDurationUs();
+        return currentTimestampUs >= getDurationUs();
     }
 
     @Override
@@ -111,6 +116,6 @@ public class MutedAudioDataSource implements DataSource {
 
     @Override
     public void rewind() {
-        // Nothing to do
+        currentTimestampUs = 0;
     }
 }
