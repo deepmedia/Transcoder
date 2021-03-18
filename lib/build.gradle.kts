fa07f923182bd0c10b144066f0dd5cb3a1e1f077
Mattia Iavarone <mat.iavarone@gmail.com>
2021-03-18 20:45:05 +0100

    Release v0.10.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         setMinSdkVersion(property("minSdkVersion") as Int)
         setTargetSdkVersion(property("targetSdkVersion") as Int)
         versionCode = 1
-        versionName = "0.9.1"
+        versionName = "0.10.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes["release"].isMinifyEnabled = false
