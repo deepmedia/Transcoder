23787f3b6728788f3802b0811f0ac1dcb06b7f74
Mattia Iavarone <mat.iavarone@gmail.com>
2021-03-30 20:03:09 +0200

    Fix encoder buffer leak

--- lib/src/main/java/com/otaliastudios/transcoder/internal/codec/Decoder.kt
+++ lib/src/main/java/com/otaliastudios/transcoder/internal/codec/Decoder.kt
@@ -3,6 +3,7 @@ package com.otaliastudios.transcoder.internal.codec
 import android.media.MediaCodec.*
 import android.media.MediaFormat
 import android.view.Surface
+import com.otaliastudios.transcoder.common.trackType
 import com.otaliastudios.transcoder.internal.data.ReaderChannel
 import com.otaliastudios.transcoder.internal.data.ReaderData
 import com.otaliastudios.transcoder.internal.media.MediaCodecBuffers
@@ -11,7 +12,11 @@ import com.otaliastudios.transcoder.internal.pipeline.Channel
 import com.otaliastudios.transcoder.internal.pipeline.QueuedStep
 import com.otaliastudios.transcoder.internal.pipeline.State
 import com.otaliastudios.transcoder.internal.utils.Logger
+import com.otaliastudios.transcoder.internal.utils.trackMapOf
 import java.nio.ByteBuffer
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.properties.Delegates
+import kotlin.properties.Delegates.observable
 
 
 internal open class DecoderData(
@@ -30,15 +35,27 @@ internal class Decoder(
         continuous: Boolean, // relevant if the source sends no-render chunks. should we compensate or not?
 ) : QueuedStep<ReaderData, ReaderChannel, DecoderData, DecoderChannel>(), ReaderChannel {
 
-    private val log = Logger("Decoder")
+    companion object {
+        private val ID = trackMapOf(AtomicInteger(0), AtomicInteger(0))
+    }
+
+    private val log = Logger("Decoder(${format.trackType},${ID[format.trackType].getAndIncrement()})")
     override val channel = this
+
     private val codec = createDecoderByType(format.getString(MediaFormat.KEY_MIME)!!)
     private val buffers by lazy { MediaCodecBuffers(codec) }
     private var info = BufferInfo()
     private val dropper = DecoderDropper(continuous)
 
+    private var dequeuedInputs by observable(0) { _, _, _ -> printDequeued() }
+    private var dequeuedOutputs by observable(0) { _, _, _ -> printDequeued() }
+    private fun printDequeued() {
+        // log.v("dequeuedInputs=$dequeuedInputs dequeuedOutputs=$dequeuedOutputs")
+    }
+
     override fun initialize(next: DecoderChannel) {
         super.initialize(next)
+        log.i("initialize()")
         val surface = next.handleSourceFormat(format)
         codec.configure(format, surface, null, 0)
         codec.start()
@@ -46,16 +63,24 @@ internal class Decoder(
 
     override fun buffer(): Pair<ByteBuffer, Int>? {
         val id = codec.dequeueInputBuffer(0)
-        log.v("buffer(): id=$id")
-        return if (id >= 0) buffers.getInputBuffer(id) to id else null
+        return if (id >= 0) {
+            dequeuedInputs++
+            buffers.getInputBuffer(id) to id
+        } else {
+            log.i("buffer() failed. dequeuedInputs=$dequeuedInputs dequeuedOutputs=$dequeuedOutputs")
+            null
+        }
     }
 
     override fun enqueueEos(data: ReaderData) {
+        log.i("enqueueEos()!")
+        dequeuedInputs--
         val flag = BUFFER_FLAG_END_OF_STREAM
         codec.queueInputBuffer(data.id, 0, 0, 0, flag)
     }
 
     override fun enqueue(data: ReaderData) {
+        dequeuedInputs--
         val (chunk, id) = data
         val flag = if (chunk.keyframe) BUFFER_FLAG_SYNC_FRAME else 0
         codec.queueInputBuffer(id, chunk.buffer.position(), chunk.buffer.remaining(), chunk.timeUs, flag)
@@ -65,12 +90,17 @@ internal class Decoder(
     override fun drain(): State<DecoderData> {
         val result = codec.dequeueOutputBuffer(info, 0)
         return when (result) {
-            INFO_TRY_AGAIN_LATER -> State.Wait
+            INFO_TRY_AGAIN_LATER -> {
+                log.i("drain(): got INFO_TRY_AGAIN_LATER, waiting.")
+                State.Wait
+            }
             INFO_OUTPUT_FORMAT_CHANGED -> {
+                log.i("drain(): got INFO_OUTPUT_FORMAT_CHANGED, handling format and retrying. format=${codec.outputFormat}")
                 next.handleRawFormat(codec.outputFormat)
                 State.Retry
             }
             INFO_OUTPUT_BUFFERS_CHANGED -> {
+                log.i("drain(): got INFO_OUTPUT_BUFFERS_CHANGED, retrying.")
                 buffers.onOutputBuffersChanged()
                 State.Retry
             }
@@ -78,23 +108,25 @@ internal class Decoder(
                 val isEos = info.flags and BUFFER_FLAG_END_OF_STREAM != 0
                 val timeUs = if (isEos) 0 else dropper.output(info.presentationTimeUs)
                 if (timeUs != null /* && (isEos || info.size > 0) */) {
+                    dequeuedOutputs++
                     val buffer = buffers.getOutputBuffer(result)
                     val data = DecoderData(buffer, timeUs) {
                         codec.releaseOutputBuffer(result, it)
+                        dequeuedOutputs--
                     }
-                    // log.w("TDBG isEos=$isEos timeUs=$timeUs")
                     if (isEos) State.Eos(data) else State.Ok(data)
                 } else {
                     codec.releaseOutputBuffer(result, false)
                     State.Wait
+                }.also {
+                    log.v("drain(): returning $it")
                 }
             }
-        }.also {
-            log.v("Returning $it")
         }
     }
 
     override fun release() {
+        log.i("release(): releasing codec. dequeuedInputs=$dequeuedInputs dequeuedOutputs=$dequeuedOutputs")
         codec.stop()
         codec.release()
     }
