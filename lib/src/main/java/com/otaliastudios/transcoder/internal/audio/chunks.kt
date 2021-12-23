3038ecf5d4f8babe81fce8cc711c07edd28c3f98
kazuya okamoto <kazuya.226@studist.jp>
2021-12-23 16:31:22 +0900

    Fix when it is not buffer.hasRemaining

--- lib/src/main/java/com/otaliastudios/transcoder/internal/audio/chunks.kt
+++ lib/src/main/java/com/otaliastudios/transcoder/internal/audio/chunks.kt
@@ -25,8 +25,9 @@ internal class ChunkQueue(private val sampleRate: Int, private val channels: Int
     fun isEmpty() = queue.isEmpty()
 
     fun enqueue(buffer: ShortBuffer, timeUs: Long, timeStretch: Double, release: () -> Unit) {
-        require(buffer.hasRemaining())
-        queue.addLast(Chunk(buffer, timeUs, timeStretch, release))
+        if (buffer.hasRemaining()) {
+            queue.addLast(Chunk(buffer, timeUs, timeStretch, release))
+        }
     }
 
     fun enqueueEos() {
