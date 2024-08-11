533fdae8d42d046fc912d5e9926db75ae64b283b
Mattia Iavarone <mattia@deepmedia.io>
2024-08-11 20:24:42 +0200

    Allow SpeedTimeInterpolator reuse

--- lib/src/main/java/com/otaliastudios/transcoder/time/SpeedTimeInterpolator.java
+++ lib/src/main/java/com/otaliastudios/transcoder/time/SpeedTimeInterpolator.java
@@ -38,7 +38,7 @@ public class SpeedTimeInterpolator implements TimeInterpolator {
      * @return the factor
      */
     @SuppressWarnings("unused")
-    public float getFactor() {
+    public float getFactor(@NonNull TrackType type, long time) {
         return (float) mFactor;
     }
 
@@ -50,7 +50,7 @@ public class SpeedTimeInterpolator implements TimeInterpolator {
             data.lastCorrectedTime = time;
         } else {
             long realDelta = time - data.lastRealTime;
-            long correctedDelta = (long) ((double) realDelta / mFactor);
+            long correctedDelta = (long) ((double) realDelta / getFactor(type, time));
             data.lastRealTime = time;
             data.lastCorrectedTime += correctedDelta;
         }
