9a57b52c2897aa723f600decf9408ca791306a79
Mattia Iavarone <mattia@deepmedia.io>
2024-09-07 19:51:35 +0200

    Bump version to 0.11.1

--- lib-legacy/build.gradle.kts
+++ lib-legacy/build.gradle.kts
@@ -28,7 +28,7 @@ deployer {
     projectInfo {
         groupId = "com.otaliastudios"
         artifactId = "transcoder"
-        release.version = "0.11.0" // change :lib and README
+        release.version = "0.11.1" // change :lib and README
         description = "Accelerated video compression and transcoding on Android using MediaCodec APIs (no FFMPEG/LGPL licensing issues). Supports cropping to any dimension, concatenation, audio processing and much more."
         url = "https://opensource.deepmedia.io/transcoder"
         scm.fromGithub("deepmedia", "Transcoder")
